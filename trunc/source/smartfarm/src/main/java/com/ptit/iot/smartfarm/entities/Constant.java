ad0645319099399090054d009a47b825fd7dfaa7
kt1755 <thuandbanbe@gmail.com>
2017-04-25 22:14:49 +0700

    Fix interface

--- trunc/source/smartfarm/src/main/java/com/ptit/iot/smartfarm/entities/Constant.java
+++ trunc/source/smartfarm/src/main/java/com/ptit/iot/smartfarm/entities/Constant.java
@@ -2,7 +2,7 @@ package com.ptit.iot.smartfarm.entities;
 
 public class Constant {
 
-	public interface WEATHER {
+	public static class WEATHER {
 		public static final String WEATHER_UNDERGROUND_KEY = "f12fb84de226aeeb";
 	}
 }
